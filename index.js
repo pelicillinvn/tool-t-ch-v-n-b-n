54421ed70c8ececb5b5af9b280e9fdc17f9ec970
Tiến Đạt <69143145+pelicillinvn@users.noreply.github.com>
2023-01-13 14:35:40 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 //document.getElementById("result").innerHTML = myArray[0];
 const buttonSubmit= document.getElementById('button_submit');
 buttonSubmit.onclick = function() {
-const paragraphText = document.getElementById('input_text').value.trim();
+const paragraphText = document.getElementById('input_text').value.trim().replace(/\r?\n/g, "");
 const amountWord = document.getElementById("amount_word").value;
-console.log(amountWord )
-console.log(paragraphText)
+//console.log(amountWord );
+console.log(paragraphText);
 const paragraphArray = paragraphText.split('.');
 const lengthParagraph = paragraphArray.length; 
-//console.log(lengthParagraph);
+console.log(paragraphArray);
 
     console.log('click')
     for(let i=0; i<lengthParagraph; i++) {
@@ -18,28 +18,37 @@ const lengthParagraph = paragraphArray.length;
         //console.log(sentenceText)
         const wordArray = sentenceText.split(' ');
         const arrayLength= wordArray.length;
-        var z = (arrayLength- (arrayLength % amountWord))/amountWord;
+        var r = arrayLength % amountWord;
+        var z = (arrayLength- r)/amountWord;
        // console.log('z:', z)
        var resultArray=[];
-       if (z>1) {
-        
-           for (let a= 0; a < z-1; a++) {
-              const sentence = wordArray.slice(a*amountWord, (a+1)*amountWord);
-              const resultSentence = sentence.join(' ');
-             //document.getElementById('result').innerHTML=`<p>${resultSentence}</p>`;
-              resultArray.push(resultSentence); 
-              //console.log("resultArrayfor",resultArray);
-          }
-       } else {
-          resultArray = wordArray.slice(0,amountWord).join(' ');
+       if (r == 0) {
+        for (let t= 0; t < z; t++) {
+            const sentence = wordArray.slice(t*amountWord, (t+1)*amountWord);
+            const resultSentence = sentence.join(' ');
+           //document.getElementById('result').innerHTML=`<p>${resultSentence}</p>`;
+            resultArray.push(resultSentence); 
+       } }  else {
+
+           if (z>1) {
+            
+               for (let a= 0; a < z-1; a++) {
+                  const sentence = wordArray.slice(a*amountWord, (a+1)*amountWord);
+                  const resultSentence = sentence.join(' ');
+                 //document.getElementById('result').innerHTML=`<p>${resultSentence}</p>`;
+                  resultArray.push(resultSentence); 
+                  //console.log("resultArrayfor",resultArray);
+              }
+           } else {
+            resultArray.push(wordArray.join(' ')); 
+
+           }
+           const lastResult = wordArray.slice((z-1)*amountWord).join(" ");
+           //console.log("lastResult", lastResult)
+            resultArray.push(lastResult);
        }
-       const lastResult = wordArray.slice((z-1)*amountWord).join(" ");
-       console.log("lastResult", lastResult)
-        resultArray.push(lastResult);
         console.log('resultArray',resultArray);
         for(let b=0; b<resultArray.length; b++) {
-            console.log(`${resultArray[b]}`);
-            // 
              const p =document.createElement('p');
              p.textContent= resultArray[b];
              document.getElementById('result').append(p);
